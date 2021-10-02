563343581d9d08c2dede488de1732d60795f0034
Sarthak Garg <sarthakgarg97@hotmail.com>
2021-10-02 15:23:42 +1000

    Improving JS code readibility + layout

--- calculator.js
+++ calculator.js
@@ -1,22 +1,28 @@
-let inputDisplay = document.getElementById("inputDisplay");
+//DOM Inputs
+let mainDisplay = document.getElementById("inputDisplay");
 let calcDisplay = document.getElementById("calcHistoryDisplay");
-let currentNumber = inputDisplay.textContent;
+let numberButtons = document.getElementsByClassName("number");
+let operatorButtons = document.getElementsByClassName("operator");
+let clearButton = document.getElementById("clearBtn");
+let backspaceButton = document.getElementById("backspaceBtn");
+let equalsButton = document.getElementById("equals");
+// let addButton = document.getElementById("addBtn");
+// let subtractButton = document.getElementById("subtractBtn");
+// let multiplyButton = document.getElementById("multiplyBtn");
+// let divideButton = document.getElementById("divideBtn");
+
+
+//global variable declaration
+let currentNumber = mainDisplay.textContent;
 let firstNumber = null;
 let result = null;
-let secondNumber = 0;
 let reClickFlag = false;
 let operatorUsed = "";
 let symbolOperator = "";
-let numberButtons = document.getElementsByClassName("number");
-let clearButton = document.getElementById("clearBtn");
-let backspaceButton = document.getElementById("backspaceBtn");
-let addButton = document.getElementById("addBtn");
-let subtractButton = document.getElementById("subtractBtn");
-let multiplyButton = document.getElementById("multiplyBtn");
-let divideButton = document.getElementById("divideBtn");
-let operatorButtons = document.getElementsByClassName("operator");
-let equalsButton = document.getElementById("equals");
 
+
+//functions
+//gets operator for operate function depending on button clicked or key pressed
 function getOperator(inputString){
     switch(inputString){
         case "+":
@@ -32,6 +38,7 @@ function getOperator(inputString){
     };
 };
 
+//keyBoard only - gets symbol operator to be input into calcDisplay
 function getSymbolOperator(inputString){
     switch(inputString){
         case "+":
@@ -48,30 +55,23 @@ function getSymbolOperator(inputString){
 };
 
 
-//insert numbers into input
+//insert numbers into mainDisplay
 function appendInput(){
-    inputDisplay.textContent = parseInt(currentNumber.toString() + this.textContent,10);
-    currentNumber = parseInt(inputDisplay.textContent,10);
+    mainDisplay.textContent = parseInt(currentNumber.toString() + this.textContent,10);
+    currentNumber = parseInt(mainDisplay.textContent,10);
     reClickFlag = true;
 };
+
 
 function activatekeyboardNumbers(e){
     const key = document.querySelector(`span[data-key = "${e.key}"]`);
     if (!key) return; //exits if key outside scope entered
     //console.log(e.key);
     if (Number.isNaN(parseInt(e.key,10))) return; //exits if non-number entered
-    inputDisplay.textContent = parseInt(currentNumber.toString() + e.key,10);
-    currentNumber = parseInt(inputDisplay.textContent,10);
+    mainDisplay.textContent = parseInt(currentNumber.toString() + e.key,10);
+    currentNumber = parseInt(mainDisplay.textContent,10);
     reClickFlag = true;
-    //console.log(e.key);
-   // console.log(parseInt(e.key,10));
 }
-
-for (let i=0; i< numberButtons.length;i++){
-    numberButtons[i].addEventListener("click",appendInput);
-};
-
-window.addEventListener('keydown', activatekeyboardNumbers);
 
 //clear Button
 function clearInput(){
@@ -80,38 +80,23 @@ function clearInput(){
     secondNumber = 0;
     result = null;
     calcDisplay.textContent = "";
-    inputDisplay.textContent = currentNumber;
+    mainDisplay.textContent = currentNumber;
 }
-
-
-clearButton.addEventListener("click", clearInput);
-document.addEventListener('keydown', function(event){
-	if(event.key === "Escape"){
-		clearInput();
-	}
-});
 
 //Backspace button
 function backSpaceInput(){
     if (currentNumber.toString().length ===1){
         currentNumber = 0;
-        inputDisplay.textContent = currentNumber;
+        mainDisplay.textContent = currentNumber;
     } else{
         currentNumber = parseInt(currentNumber.toString().substring(0,currentNumber.toString().length-1),10);
-        inputDisplay.textContent = currentNumber;
+        mainDisplay.textContent = currentNumber;
     };
     
 };
-backspaceButton.addEventListener("click", backSpaceInput);
 
-document.addEventListener('keydown', (e) => {
-    if (e.key === "Backspace") {
-        backSpaceInput();
-    };
-});
-//add operator functionality;
-
-function getOperand(){
+//operator functionality
+function appendOperand(){
     if (reClickFlag === true){
         if (firstNumber != null){
             calcResult();
@@ -123,16 +108,6 @@ function getOperand(){
     operatorUsed = getOperator(this.textContent);
     calcDisplay.textContent = firstNumber + this.textContent;
     currentNumber = 0;
-    // console.log(operatorUsed);
-    
-    // if (result != null){
-    //     calcResult();
-    //     calcDisplay.textContent = "it worked!";
-    // };
-};
-
-for (let i=0; i<operatorButtons.length; i++){
-    operatorButtons[i].addEventListener("click", getOperand);
 };
 
 function activatekeyboardOperands(e){
@@ -153,87 +128,17 @@ function activatekeyboardOperands(e){
     operatorUsed = getOperator(e.key);
     calcDisplay.textContent = firstNumber + symbolOperator;
     currentNumber = 0;
-    // console.log(operatorUsed);
-
-
-    //console.log(e.key);
-   // console.log(parseInt(e.key,10));
 }
 
-document.addEventListener('keydown', activatekeyboardOperands);
-
 function calcResult(){
-    result = operate(operatorUsed, firstNumber, currentNumber);
-    inputDisplay.textContent = result;
+    result = Math.round(operate(operatorUsed, firstNumber, currentNumber)*10000)/10000; //rounding to 4dp
+    mainDisplay.textContent = result;
     calcDisplay.textContent = firstNumber + symbolOperator + currentNumber + "=";
     currentNumber = result;
     firstNumber = null;
-    //result = null;
 };
-equalsButton.addEventListener("click", calcResult);
 
-document.addEventListener('keydown', (e) => {
-    if (e.key === "Enter") {
-        calcResult();
-    };
-})
-//operator buttons
-//add
-// addButton.addEventListener("click", () => {
-//     operatorUsed = "add";
-
-//     switch(calcDisplay.textContent) {
-//         case "":
-//             firstNumber = currentNumber;
-//             currentNumber = 0;
-//             inputDisplay.textContent = 0;
-//             calcDisplay.textContent = firstNumber + "+";
-//             console.log("case 1 applied");
-//             break;
-//         case (firstNumber + "+"):
-//             secondNumber = currentNumber;
-//             result = operate("add", firstNumber, secondNumber);
-//             calcDisplay.textContent = firstNumber + "+" + secondNumber;
-//             inputDisplay.textContent = result;
-//             currentNumber = 0;
-//             console.log("case 2 applied");
-//             break;
-//         case (firstNumber + "+" + secondNumber):
-//             result = firstNumber;
-//             calcDisplay.textContent = firstNumber + "+";
-//             currentNumber = 0;
-//             inputDisplay.textContent = 0;
-//             result = null;
-//             console.log("case 3 applied");
-//             break;
-//         default:
-//             inputDisplay.textContent = "how did you manage this?";
-//             console.log("case default applied");
-//     };
-
-    // if (!firstNumber) {
-    //     firstNumber = currentNumber;
-    //     currentNumber = 0;
-    //     inputDisplay.textContent = 0;
-    //     calcDisplay.textContent = firstNumber + "+";
-    // } else{
-    //     result = operate(operatorUsed, firstNumber, currentNumber);
-    //     calcDisplay.textContent = firstNumber + "+" + currentNumber;
-    //     inputDisplay.textContent = result;
-    //     currentNumber = 0;
-    //     firstNumber = result;
-    //  //   result = null;
-    // }
-    
-
-    
-    
-// });
-
-
-
-// console.log(numberButtons[0].textContent);
-// console.log(inputDisplay);
+//basic functions
 function func_add(a,b){
     return a+b;
 };
@@ -250,10 +155,41 @@ function func_divide(a,b){
     return a/b;
 };
 
-function func_power(a,b){
-    return Math.pow(a,b);
-};
-
 function operate(functionName, a, b){
     return window["func_"+functionName](a,b);
 };
+
+//Click event listeners
+for (let i=0; i< numberButtons.length;i++){
+    numberButtons[i].addEventListener("click",appendInput);
+};
+
+for (let i=0; i<operatorButtons.length; i++){
+    operatorButtons[i].addEventListener("click", appendOperand);
+};
+
+clearButton.addEventListener("click", clearInput);
+backspaceButton.addEventListener("click", backSpaceInput);
+equalsButton.addEventListener("click", calcResult);
+
+
+//keyboard Event listeners
+window.addEventListener('keydown', activatekeyboardNumbers);
+document.addEventListener('keydown', activatekeyboardOperands);
+document.addEventListener('keydown', (e) => {
+    (e.key === "Enter") ? calcResult()
+    : (e.key === "Backspace") ? backSpaceInput()
+    : (e.key === "Escape") ? clearInput()
+    : null;
+});
+
+
+// document.addEventListener('keydown', function(event){
+// 	if(event.key === "Escape"){
+// 		clearInput();
+// 	} else if (event.key === "Backspace") {
+//         backSpaceInput();
+//     } else if (event.key === "Enter") {
+//         calcResult();
+//     };
+// });
